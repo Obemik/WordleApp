9905f846709f088cb423c3ca626e30d7a21ec65c
Obemik <misaperepecaj@gmail.com>
2025-06-16 16:48:49 +0300

    Remove unused helper import from AuthenticationViewModel

--- WordleApp/ViewModels/AuthenticationViewModel.cs
+++ WordleApp/ViewModels/AuthenticationViewModel.cs
@@ -2,7 +2,6 @@ using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
 using WordleApp.Services;
-using WordleApp.Helpers;
 using CommunityToolkit.Mvvm.Input;
 using Microsoft.Extensions.Logging;
 
